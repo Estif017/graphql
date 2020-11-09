152f0ee8dcb8d1719e7f7093a9dedc278dfcc7c4
Estif017 <estifoteklearegay@gmail.com>
2020-11-08 23:25:08 -0800

    connected our server with mongoose/database and retrive it's data with apollo server

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const {ApolloServer} = require('apollo-server')
 const gql=require('graphql-tag')
+const mongoose = require('mongoose')
+
+const {MONGODB}=require('./config')
+const Post=require('./models/Post')
 
 const typeDefs=gql`
+    type Post{
+        id:ID!,
+        body:String!,
+        username:String!,
+        createdAt:String!
+    }
     type Query{
-        sayHi:String!
+        getPost:[Post]
     }
 
 `
 
 const resolvers={
     Query:{
-        sayHi:()=>{return"Hello World"}
+        async getPost(){
+            try {
+                const posts = await Post.find()
+                return posts   
+            } catch (error) {
+                throw new Error(error)
+            }
+        }
     }
 }
+
+mongoose.
+    connect(MONGODB,{useNewUrlParser:true,useUnifiedTopology: true })
+    .then(()=>{
+        console.log(`Mongodb connected`);
+        return server.listen({port:5000})
+    })
+    .then(res=>console.log(`Server is running at port ${res.url}`))
 
 const server=new ApolloServer({
     typeDefs,resolvers
 })
-
-server.listen({port:5000}).then(res=>console.log(`Server is running at port ${res.url}`))

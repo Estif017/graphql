e3fd6ef78d79409d63ce860e2daf6279fdc9cabf
Estif017 <estifoteklearegay@gmail.com>
2020-11-08 22:57:09 -0800

    setting up gql apollo server

---
+++ index.js
@@ -0,0 +1,21 @@
+const {ApolloServer} = require('apollo-server')
+const gql=require('graphql-tag')
+
+const typeDefs=gql`
+    type Query{
+        sayHi:String!
+    }
+
+`
+
+const resolvers={
+    Query:{
+        sayHi:()=>{return"Hello World"}
+    }
+}
+
+const server=new ApolloServer({
+    typeDefs,resolvers
+})
+
+server.listen({port:5000}).then(res=>console.log(`Server is running at port ${res.url}`))
